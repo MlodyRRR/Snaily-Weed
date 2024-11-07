a8996f58f16d226188578401caf2eb903f509efe
MlodyR <147008169+MlodyRRR@users.noreply.github.com>
2024-11-07 01:09:47 +0100

    Add files via upload

--- client.lua
+++ client.lua
@@ -3,97 +3,76 @@ local PlantObjects = {}
 
 local function PlayAnim(animData)
     if not animData or not animData.dict or not animData.anim then return end
-
     RequestAnimDict(animData.dict)
-    while not HasAnimDictLoaded(animData.dict) do
-        Wait(10)
-    end
-
+    while not HasAnimDictLoaded(animData.dict) do Wait(10) end
     TaskPlayAnim(PlayerPedId(), animData.dict, animData.anim, 8.0, -8.0, -1, animData.flag or 1, 0, false, false, false)
 end
 
 local function CleanupPlantObjects()
     for _, object in pairs(PlantObjects) do
-        if DoesEntityExist(object) then
-            DeleteEntity(object)
-        end
+        if DoesEntityExist(object) then DeleteEntity(object) end
     end
     PlantObjects = {}
 end
 
 local function WeedDestroy(plantId)
     if not WeedPlants[plantId] then return end
+    local plantCoords = WeedPlants[plantId].coords
 
-    lib.callback('snaily_weed:checkLighter', false, function(hasLighter, error)
-        if not hasLighter then
-            lib.notify({
-                type = 'error',
-                title = 'Błąd',
-                description = error or 'Potrzebujesz zapalniczki!'
-            })
-            return
-        end
+    if exports.ox_inventory:Search('count', Config.Items.Lighter) < 1 then
+        lib.notify({ type = 'error', description = 'Potrzebujesz zapalniczki!' })
+        return
+    end
 
-        local plantCoords = WeedPlants[plantId].coords
+    PlayAnim(Config.Animations.Destroying)
 
-        PlayAnim(Config.Animations.Destroying)
-        if lib.progressBar({
+    if lib.progressBar({
             duration = Config.Progress.Destroying.duration,
             label = Config.Progress.Destroying.label,
             useWhileDead = false,
             canCancel = true,
-            disable = {
-                car = true,
-                move = true,
-                combat = true
-            }
+            disable = { car = true, move = true, combat = true }
         }) then
-            lib.callback('snaily_weed:checkLighter', false, function(stillHasLighter)
-                if not stillHasLighter then
-                    lib.notify({
-                        type = 'error',
-                        title = 'Błąd',
-                        description = 'Potrzebujesz zapalniczki!'
-                    })
-                    ClearPedTasks(PlayerPedId())
-                    return
-                end
-
-                RequestNamedPtfxAsset(Config.DestroyEffect.dict)
-                while not HasNamedPtfxAssetLoaded(Config.DestroyEffect.dict) do
-                    Wait(0)
-                end
-
-                UseParticleFxAssetNextCall(Config.DestroyEffect.dict)
-                local fire = StartParticleFxLoopedAtCoord(
-                    Config.DestroyEffect.name,
-                    plantCoords.x + Config.DestroyEffect.offset.x,
-                    plantCoords.y + Config.DestroyEffect.offset.y,
-                    plantCoords.z + Config.DestroyEffect.offset.z,
-                    0.0, 0.0, 0.0,
-                    Config.DestroyEffect.scale,
-                    false, false, false
-                )
-
-                Wait(math.floor(Config.DestroyEffect.duration * 0.66))
-
-                lib.callback('snaily_weed:WeedDestroy', false, function(success)
-                    if success then
-                        Wait(math.floor(Config.DestroyEffect.duration * 0.34))
-                        StopParticleFxLooped(fire, 0)
-                        RemoveNamedPtfxAsset(Config.DestroyEffect.dict)
-                        lib.notify(Config.Notify.Success.Destroyed)
-                    end
-                end, plantId)
-            end)
+        if exports.ox_inventory:Search('count', Config.Items.Lighter) < 1 then
+            lib.notify({ type = 'error', description = 'Potrzebujesz zapalniczki!' })
+            ClearPedTasks(PlayerPedId())
+            return
         end
-        ClearPedTasks(PlayerPedId())
-    end)
+
+        RequestNamedPtfxAsset(Config.DestroyEffect.dict)
+        while not HasNamedPtfxAssetLoaded(Config.DestroyEffect.dict) do Wait(0) end
+
+        UseParticleFxAssetNextCall(Config.DestroyEffect.dict)
+        local fire = StartParticleFxLoopedAtCoord(
+            Config.DestroyEffect.name,
+            plantCoords.x + Config.DestroyEffect.offset.x,
+            plantCoords.y + Config.DestroyEffect.offset.y,
+            plantCoords.z + Config.DestroyEffect.offset.z,
+            0.0, 0.0, 0.0,
+            Config.DestroyEffect.scale,
+            false, false, false
+        )
+
+        Wait(math.floor(Config.DestroyEffect.duration * 0.66))
+
+        lib.callback('snaily_weed:WeedDestroy', false, function(success)
+            if success then
+                Wait(math.floor(Config.DestroyEffect.duration * 0.34))
+                StopParticleFxLooped(fire, 0)
+                RemoveNamedPtfxAsset(Config.DestroyEffect.dict)
+                lib.notify({ type = 'success', description = 'Zniszczono roślinę' })
+            end
+        end, plantId)
+    end
+    ClearPedTasks(PlayerPedId())
 end
 
 local function WeedMenu(plantId)
     local plant = WeedPlants[plantId]
     if not plant then return end
+
+    local hasWater = exports.ox_inventory:Search('count', Config.Items.Water) > 0
+    local hasFertilizer = exports.ox_inventory:Search('count', Config.Items.Fertilizer) > 0
 
     local options = {
         {
@@ -109,7 +88,9 @@ local function WeedMenu(plantId)
             progress = math.floor(plant.water or 0),
             colorScheme = 'green',
             icon = 'droplet',
+            disabled = not hasWater,
             onSelect = function()
+                if not hasWater then return end
                 lib.callback('snaily_weed:waterPlant', false, function(success)
                     if success then
                         PlayAnim(Config.Animations.Watering)
@@ -125,7 +106,9 @@ local function WeedMenu(plantId)
             progress = math.floor(plant.fertilizer or 0),
             colorScheme = 'green',
             icon = 'seedling',
+            disabled = not hasFertilizer,
             onSelect = function()
+                if not hasFertilizer then return end
                 lib.callback('snaily_weed:fertilizePlant', false, function(success)
                     if success then
                         PlayAnim(Config.Animations.Fertilizing)
@@ -146,13 +129,12 @@ local function WeedMenu(plantId)
             title = 'Zniszcz roślinę',
             description = 'Spal roślinę',
             icon = 'fire',
-            onSelect = function()
-                WeedDestroy(plantId)
-            end
+            onSelect = function() WeedDestroy(plantId) end
         },
         {
             title = 'Zbierz roślinę',
-            description = math.floor(plant.growth or 0) >= 100 and 'Roślina gotowa do zbioru' or 'Roślina nie jest jeszcze gotowa',
+            description = math.floor(plant.growth or 0) >= 100 and 'Roślina gotowa do zbioru' or
+                'Roślina nie jest jeszcze gotowa',
             icon = 'cannabis',
             disabled = math.floor(plant.growth or 0) < 100,
             onSelect = function()
@@ -163,7 +145,7 @@ local function WeedMenu(plantId)
 
                     lib.callback('snaily_weed:harvestPlant', false, function(success)
                         if success then
-                            lib.notify(Config.Notify.Success.Harvested)
+                            lib.notify({ type = 'success', description = 'Zebrano roślinę' })
                         end
                     end, plantId)
                 end
@@ -171,58 +153,79 @@ local function WeedMenu(plantId)
         }
     }
 
-    lib.registerContext({
-        id = 'plant_menu',
-        title = 'Stan rośliny',
-        options = options
-    })
-
+    lib.registerContext({ id = 'plant_menu', title = 'Stan rośliny', options = options })
     lib.showContext('plant_menu')
+end
+
+local function UpdatePlantModel(plantId, plant)
+    if PlantObjects[plantId] and DoesEntityExist(PlantObjects[plantId]) then
+        DeleteEntity(PlantObjects[plantId])
+    end
+
+    local modelInfo
+    if plant.growth < 33 then
+        modelInfo = Config.PlantModels[1]
+    elseif plant.growth < 66 then
+        modelInfo = Config.PlantModels[2]
+    else
+        modelInfo = Config.PlantModels[3]
+    end
+
+    local ground, groundZ = GetGroundZFor_3dCoord(plant.coords.x, plant.coords.y, plant.coords.z, true)
+    if not ground then groundZ = plant.coords.z end
+
+    local object = CreateObject(
+        GetHashKey(modelInfo.model),
+        plant.coords.x,
+        plant.coords.y,
+        groundZ + modelInfo.zOffset,
+        false, false, false
+    )
+
+    FreezeEntityPosition(object, true)
+    PlaceObjectOnGroundProperly(object)
+    PlantObjects[plantId] = object
+
+    exports.ox_target:addLocalEntity(object, {
+        {
+            name = 'check_plant',
+            icon = Config.Target.icon,
+            label = Config.Target.label,
+            distance = Config.Target.distance,
+            onSelect = function() WeedMenu(plantId) end
+        }
+    })
 end
 
 RegisterNetEvent('snaily_weed:useSeed', function()
     PlayAnim(Config.Animations.Planting)
-
     if lib.progressBar(Config.Progress.Planting) then
-        ClearPedTasks(PlayerPedId())
         local coords = GetEntityCoords(PlayerPedId())
-
         lib.callback('snaily_weed:plantSeed', false, function(success)
             if success then
-                lib.notify(Config.Notify.Success.Planted)
+                lib.notify({ type = 'success', description = 'Zasadzono roślinę' })
             end
         end, coords)
-    else
-        ClearPedTasks(PlayerPedId())
     end
+    ClearPedTasks(PlayerPedId())
 end)
 
 RegisterNetEvent('snaily_weed:syncPlants', function(serverPlants)
     CleanupPlantObjects()
     WeedPlants = serverPlants
-
     for plantId, plant in pairs(WeedPlants) do
-        local object = CreateObject(Config.Plant.Model, plant.coords.x, plant.coords.y,
-            plant.coords.z + Config.Plant.ZOffset, false, false, false)
-        FreezeEntityPosition(object, true)
-        plant.object = object
-        table.insert(PlantObjects, object)
+        UpdatePlantModel(plantId, plant)
+    end
+end)
 
-        exports.ox_target:addLocalEntity(object, {
-            {
-                name = 'check_plant',
-                icon = Config.Target.icon,
-                label = Config.Target.label,
-                distance = Config.Target.distance,
-                onSelect = function()
-                    WeedMenu(plantId)
-                end
-            }
-        })
+RegisterNetEvent('snaily_weed:updatePlant', function(plantId, plant)
+    if WeedPlants[plantId] then
+        WeedPlants[plantId] = plant
+        UpdatePlantModel(plantId, plant)
     end
 end)
 
 AddEventHandler('onResourceStop', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then return end
+    if GetCurrentResourceName() ~= resourceName then return end
     CleanupPlantObjects()
 end)
--- config.lua
+++ config.lua
@@ -1,129 +1,87 @@
 Config = {}
 
-Config.Items = {
-    Seed = 'weed_seed',
-    Water = 'weed_water',
-    Fertilizer = 'weed_fertilizer',
-    Weed = 'weed',
-    Lighter = 'lighter'
-}
+Config = {
+    Items = {
+        Seed = 'weed_seed',
+        Water = 'weed_water',
+        Fertilizer = 'weed_fertilizer',
+        Weed = 'weed',
+        Lighter = 'lighter'
+    },
 
-Config.Plant = {
-    GrowthTime = 0.5,          -- Czas w minutach do pełnego wzrostu
-    WaterDecrease = 0.1,       -- Spadek wody na sekundę
-    FertilizerDecrease = 0.05, -- Spadek nawozu na sekundę
-    QualityIncrease = 5,       -- Wzrost jakości przy podlewaniu/nawożeniu
-    StartingValues = {
-        Water = 100,
-        Fertilizer = 100,
-        Quality = 100
-    },
-    Model = "prop_weed_02",
-    ZOffset = -1.0
-}
-
-Config.Harvest = {
-    Min = 1,
-    Max = 3,
-    QualityMultiplier = true
-}
-
-Config.Animations = {
-    Planting = {
-        dict = "timetable@gardener@filling_can",
-        anim = "gar_ig_5_filling_can",
-        flag = 1
-    },
-    Watering = {
-        dict = "timetable@gardener@filling_can",
-        anim = "gar_ig_5_filling_can",
-        flag = 1
-    },
-    Fertilizing = {
-        dict = "timetable@gardener@filling_can",
-        anim = "gar_ig_5_filling_can",
-        flag = 1
-    },
-    Harvesting = {
-        dict = "mini@repair",
-        anim = "fixing_a_player",
-        flag = 1
-    },
-    Destroying = {
-        dict = "amb@world_human_stand_fire@male@base",
-        anim = "base",
-        flag = 49
-    }
-}
-
-Config.Progress = {
-    Planting = {
-        label = 'Sadzenie rośliny...',
-        duration = 7500
-    },
-    Watering = {
-        label = 'Podlewanie...',
-        duration = 3000
-    },
-    Fertilizing = {
-        label = 'Nawożenie...',
-        duration = 3000
-    },
-    Harvesting = {
-        label = 'Zbieranie...',
-        duration = 5000
-    },
-    Destroying = {
-        label = 'Podpalanie rośliny...',
-        duration = 5000
-    }
-}
-
-Config.DestroyEffect = {
-    dict = "core",
-    name = "ent_sht_petrol_fire",
-    scale = 2.0,
-    duration = 3000,
-    offset = {
-        x = 0.0,
-        y = 0.0,
-        z = -0.5
-    }
-}
-
-Config.Notify = {
-    Success = {
-        Planted = {
-            title = 'Sukces',
-            description = 'Zasadzono roślinę'
-        },
-        Watered = {
-            title = 'Sukces',
-            description = 'Podlano roślinę'
-        },
-        Fertilized = {
-            title = 'Sukces',
-            description = 'Nawożono roślinę'
-        },
-        Harvested = {
-            title = 'Sukces',
-            description = 'Zebrano roślinę'
-        },
-        Destroyed = {
-            title = 'Sukces',
-            description = 'Zniszczono roślinę'
+    Plant = {
+        GrowthTime = 3,
+        WaterDecrease = 0.1,
+        FertilizerDecrease = 0.05,
+        QualityIncrease = 5,
+        StartingValues = {
+            Water = 100,
+            Fertilizer = 100,
+            Quality = 100
         }
     },
-    Error = {
-        NoLighter = {
-            title = 'Błąd',
-            description = 'Potrzebujesz zapalniczki!'
-        }
-    }
-}
 
-Config.Target = {
-    icon = 'fas fa-cannabis',
-    label = 'Sprawdź roślinę',
-    distance = 2.0
+    PlantingZones = {
+        {
+            coords = vector3(2220.12, 5577.34, 53.84),
+            radius = 50.0,
+            name = "Pole 1"
+        },
+        {
+            coords = vector3(2125.45, 5625.12, 54.20),
+            radius = 30.0,
+            name = "Pole 2"
+        }
+    },
+
+    PlantModels = {
+        [1] = {
+            model = "bkr_prop_weed_01_small_01c",
+            zOffset = -1.0
+        },
+        [2] = {
+            model = "bkr_prop_weed_med_01a",
+            zOffset = -1.0
+        },
+        [3] = {
+            model = "bkr_prop_weed_lrg_01a",
+            zOffset = -1.0
+        }
+    },
+
+    Harvest = {
+        Min = 1,
+        Max = 3,
+        QualityMultiplier = true
+    },
+
+    Animations = {
+        Planting = { dict = "timetable@gardener@filling_can", anim = "gar_ig_5_filling_can", flag = 1 },
+        Watering = { dict = "timetable@gardener@filling_can", anim = "gar_ig_5_filling_can", flag = 1 },
+        Fertilizing = { dict = "timetable@gardener@filling_can", anim = "gar_ig_5_filling_can", flag = 1 },
+        Harvesting = { dict = "mini@repair", anim = "fixing_a_player", flag = 1 },
+        Destroying = { dict = "amb@world_human_stand_fire@male@base", anim = "base", flag = 49 }
+    },
+
+    Progress = {
+        Planting = { label = 'Sadzenie rośliny...', duration = 7500 },
+        Watering = { label = 'Podlewanie...', duration = 3000 },
+        Fertilizing = { label = 'Nawożenie...', duration = 3000 },
+        Harvesting = { label = 'Zbieranie...', duration = 5000 },
+        Destroying = { label = 'Podpalanie rośliny...', duration = 5000 }
+    },
+
+    DestroyEffect = {
+        dict = "core",
+        name = "ent_sht_petrol_fire",
+        scale = 2.0,
+        duration = 3000,
+        offset = vector3(0.0, 0.0, -0.5)
+    },
+
+    Target = {
+        icon = 'fas fa-cannabis',
+        label = 'Sprawdź roślinę',
+        distance = 2.0
+    }
 }
